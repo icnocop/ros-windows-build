1daf149cd4b335b906f548c6d788374ead5850d8
Lior Lustgarten <45467030+lilustga@users.noreply.github.com>
2021-10-14 16:04:42 -0700

    Add python executable path to cmake args

--- ros/foxy/build.bat
+++ ros/foxy/build.bat
@@ -15,6 +15,7 @@ colcon build ^
         -DBUILD_TESTING:BOOL=False ^
         -DCMAKE_PROGRAM_PATH=%INSTALL_DIR%\tools\protobuf;%INSTALL_DIR%\tools\qt5\bin ^
         -DCMAKE_PDB_OUTPUT_DIRECTORY=%PDB_OUTPUT_DIRECTORY% ^
+        -DPYTHON_EXECUTABLE=%INSTALL_DIR%\python.exe ^
     2>&1
 if errorlevel 1 exit 1
 
